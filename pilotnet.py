remove normalization, correct onnx saving

pilotnet.py
@@ -11,10 +11,8 @@ class PilotNet(nn.Module):
     Conditonal control is concatenated with input features to each policy branchy
     """
 
-    def __init__(self, scale=None):
+    def __init__(self):
         super(PilotNet, self).__init__()
-
-        self.scale = scale
 
         self.features = nn.Sequential(
             nn.Conv2d(3, 24, 5, stride=2),
@@ -50,11 +48,6 @@ class PilotNet(nn.Module):
     def forward(self, x):
         x = self.features(x)
         outs = self.regressor(x)
-
-        if self.scale and not self.training:
-            # undo normalization to [0, 1] in eval mode
-            scale = self.scale
-            outs = outs * (2*scale) - scale
         return outs
 
 
@@ -64,10 +57,8 @@ class PilotnetEBM(nn.Module):
     https://implicitbc.github.io/
     """
 
-    def __init__(self, bound: int = None):
+    def __init__(self):
         super().__init__()
-
-        self.bound = bound
 
         self.features = nn.Sequential(
             nn.Conv2d(3, 24, 5, stride=2),
@@ -109,8 +100,6 @@ class PilotnetEBM(nn.Module):
         return next(self.parameters()).device
 
     def forward(self, x, y):
-        if self.bound is not None:
-            y = (y + self.bound) / (2 * self.bound) # normalize y to [0, 1]
         logging.debug(f'x: {x.shape} {x.dtype}')
         logging.debug(f'y: {y.shape} {y.dtype}')
         out = self.features(x)
@@ -184,10 +173,9 @@ class PilotnetMDN(nn.Module):
     The rest transformed from a regressor into a discretized classification model.
     """
 
-    def __init__(self, n_gaussians=3, scale=None):
+    def __init__(self, n_gaussians=3):
         super().__init__()
 
-        self.scale = scale
         self.n_gaussians = n_gaussians
 
         self.features = nn.Sequential(
@@ -223,11 +211,5 @@ class PilotnetMDN(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        pi, sigma, mu = self.regressor(x)
-
-        if self.scale and not self.training:
-            # undo normalization to [0, 1] in eval mode
-            scale = self.scale
-            sigma = sigma * (2*scale) - scale
-            mu = mu * (2*scale) - scale
-        return pi, sigma, mu
+        outs = self.regressor(x)
+        return outs

scripts/pt_to_onnx.py
@@ -9,8 +9,9 @@ import onnx
 
 from ebm import optimizers
 from dataloading.nvidia import NvidiaValidationDataset
-from pilotnet import PilotnetEBM, PilotnetClassifier
+from pilotnet import PilotNet, PilotnetEBM, PilotnetClassifier, PilotnetMDN
 from classifier.infer import ClassifierInferrer
+from mdn.infer import MDNInferrer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -22,11 +23,12 @@ def convert_pt_to_onnx(model_path, model_type, output_path=None, config={}):
     inference_config = None
     inference_wrapper = None
 
-    if model_type == 'pilotnet-ebm':
+    if model_type == 'pilotnet':
+        model = PilotNet()
+    elif model_type == 'pilotnet-ebm':
         n_samples = config['n_samples']
         n_dfo_iters = config.get('ebm_dfo_iters', 0)
         ebm_constant_samples = config.get('ebm_constant_samples', True)
-        normalize_inputs = config.get('normalize_inputs', True)
         
         inference_config = optimizers.DerivativeFreeConfig(
             bound=steering_bound,
@@ -35,7 +37,7 @@ def convert_pt_to_onnx(model_path, model_type, output_path=None, config={}):
             iters=n_dfo_iters,
         )
         inference_wrapper = optimizers.DFOptimizerConst if ebm_constant_samples else optimizers.DFOptimizer
-        model = PilotnetEBM(bound=steering_bound if normalize_inputs else None)
+        model = PilotnetEBM()
         model = inference_wrapper(model, inference_config)
     elif model_type == 'pilotnet-classifier':
         weights = torch.load(model_path, map_location=torch.device('cpu'))
@@ -43,6 +45,10 @@ def convert_pt_to_onnx(model_path, model_type, output_path=None, config={}):
         output_layer_size = weight.shape[0]
         model = PilotnetClassifier(output_layer_size)
         model = ClassifierInferrer(model, steering_bound, output_layer_size)
+    elif model_type == 'pilotnet-mdn':
+        n_gaussians = config['n_gaussians']
+        model = PilotnetMDN(n_gaussians=n_gaussians)
+        model = MDNInferrer(model)
     else:
         raise ValueError(f'Unknown model type: {model_type}')
 
@@ -87,6 +93,7 @@ def main(raw_args=None):
     parser.add_argument('--file', type=str, help='Path to the PyTorch model')
     parser.add_argument('--model-type', type=str, help='Type of the model.', choices=['pilotnet-ebm', 'pilotnet-classifier'])
     parser.add_argument('--ebm-dfo-iters', default=0, type=int, help='Number of DFO iterations. Ignored if --with_dfo is not set.')
+    parser.add_argument('--n-gaussians', default=3, type=int, help='Number of gaussians for an MDN.')
     parser.add_argument('--n-samples', default=128, type=int, help='Number of action samples in the discretization/as input to EBM.')
     parser.add_argument('--output', default=None, type=str, help='Path to the output ONNX model')
     parser.add_argument('--steering-bound', default=4.5, type=float, help='Bound for the steering angle, in radians. If not set, the model will use the default bound.')

trainer.py
@@ -310,7 +310,7 @@ class PilotNetTrainer(Trainer):
 
         self.train_conf = kwargs['train_conf']
 
-        self.model = PilotNet(scale=self.train_conf.steering_bound if self.train_conf.normalize_targets else None)
+        self.model = PilotNet()
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.train_conf.learning_rate, betas=(0.9, 0.999),
                                            eps=1e-08, weight_decay=self.train_conf.weight_decay, amsgrad=False)
         self.model = self.model.to(self.device)
@@ -333,12 +333,18 @@ class PilotNetTrainer(Trainer):
 
     def train_batch(self, model, data, target_values, condition_mask, criterion):
         inputs = data['image'].to(self.device)
-        if self.train_conf.normalize_targets:
-            scale = self.train_conf.steering_bound
-            target_values = (target_values + scale) / (2 * scale) # normalize target to roughly [0, 1]
         target_values = target_values.to(self.device)
         predictions = model(inputs)
         return predictions, criterion(predictions, target_values)
+
+    def save_onnx(self, _, __):
+        model_type = self.train_conf.model_type
+        pt_models = [f'{self.save_dir}/last.pt', f'{self.save_dir}/best.pt']
+
+        for pt_model_path in pt_models:
+            model_path = convert_pt_to_onnx(pt_model_path, model_type)
+            if self.wandb_logging:
+                wandb.save(model_path)
 
 
 class EBMTrainer(Trainer):
@@ -356,7 +362,7 @@ class EBMTrainer(Trainer):
         super().__init__(*args, **kwargs)
 
         self.train_conf = kwargs['train_conf']
-        self.model = PilotnetEBM(self.train_conf.steering_bound)
+        self.model = PilotnetEBM()
         self.model.to(self.device)
 
         optim_config, inference_config = self._initialize_config(self.train_conf)
@@ -730,7 +736,7 @@ class MDNTrainer(Trainer):
         super().__init__(*args, **kwargs)
 
         self.train_conf = kwargs['train_conf']
-        self.model = PilotnetMDN(self.train_conf.mdn_n_components, scale=self.train_conf.steering_bound if self.train_conf.normalize_targets else None)
+        self.model = PilotnetMDN(self.train_conf.mdn_n_components)
         self.model.to(self.device)
 
         self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.train_conf.learning_rate, betas=(0.9, 0.999),
@@ -756,10 +762,6 @@ class MDNTrainer(Trainer):
         return np.array(all_predictions)
 
     def train_batch(self, _, input, target, __, ___):
-        if self.train_conf.normalize_targets:
-            scale = self.train_conf.steering_bound
-            target = (target + scale) / (2 * scale) # normalize target to roughly [0, 1]
-
         inputs = input['image'].to(self.device)
         target = target.to(self.device, torch.float32)
 
@@ -821,3 +823,16 @@ class MDNTrainer(Trainer):
         avg_mae = epoch_mae / len(iterator)
         result = np.array(all_predictions)
         return avg_mae, result
+
+    def save_onnx(self, _, __):
+        model_type = self.train_conf.model_type
+        pt_models = [f'{self.save_dir}/last.pt', f'{self.save_dir}/best.pt']
+
+        config = {
+            'n_gaussians': self.train_conf.mdn_n_components,
+        }
+
+        for pt_model_path in pt_models:
+            model_path = convert_pt_to_onnx(pt_model_path, model_type, config=config)
+            if self.wandb_logging:
+                wandb.save(model_path)